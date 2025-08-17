cfe5e27287fc92edc86890741beb9b9a790b7a97
Mohammad Arif <programmermdarif@gmail.com>
2025-08-17 16:15:26 +0600

    deleted some garbage code

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Analytics } from "@vercel/analytics/react";
-import ReduxWrapper from "../../Provider/ReduxWrapper";
 export const metadata = {
   title: "CodeKpi",
   description:
@@ -35,19 +34,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ReduxWrapper>
-      <html lang="en">
-        <head>
-          <link rel="icon" href="/favicon.ico" />
-        </head>
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
 
-        <body className="english">
-          <div> {children} </div>
-          <ToastContainer />
-          <Analytics />
-        </body>
-      </html>
-    </ReduxWrapper>
+      <body className="english">
+        <div> {children} </div>
+        <ToastContainer />
+        <Analytics />
+      </body>
+    </html>
   );
 }
 // {"orgId":"team_6iM3ycdvBV4YAVgb30qJDD6K","projectId":"prj_9PvudQcVA8xwMbK16s9mw8Mw9co9"}
